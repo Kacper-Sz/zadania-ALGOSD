7fd5a2d969e7d116e726536448f03b8fab26029e
Kacper-Sz <80749286+Kacper-Sz@users.noreply.github.com>
2024-11-26 01:19:42 +0100

    zadanie 12 komy

--- zadanie 12/funkcje.c
+++ zadanie 12/funkcje.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "funkcje.h"
 
-
+//wypisywanie listy od poczatku
 void WyswietlListeOdPoczatku(lista l)
 {
     if(l == NULL)
@@ -20,17 +20,22 @@ void WyswietlListeOdPoczatku(lista l)
     printf("\n");
 }
 
+//dodawanie elementu na poczatek listy
 void DodajNaPoczatek(lista *l, int number)
 {
+    //tworze nowy element
     lista nowa = malloc(sizeof(elListy));
 
+    //kiedy lista pusta to dodaje element
     if(*l == NULL)
     {
         nowa->klucz = number;
+        //lista ma byc cykliczna
+        //wiec nastepny to ten sam
         nowa->nast = nowa;
         *l = nowa;
     }
-    else
+    else //kiedy jest wiecej niz jeden element
     {
         nowa->klucz = (*l)->klucz;
         (*l)->klucz = number;
@@ -41,65 +46,98 @@ void DodajNaPoczatek(lista *l, int number)
 
 void DodajNaKoniec(lista *l, int number)
 {
+    //dodanie na poczatek
     DodajNaPoczatek(l, number);
+    //przesuniecie glowy na drugi
     *l = (*l)->nast;
 }
 
 void UsunPowtorzenia(lista *l, lista *q)
 {
+    //jezeli ktoras z list jest pusta to nie ma co usuwac
     if (*l == NULL || *q == NULL)
     {
         return;
     }
 
-    lista obecnyQ = *q; // Iterujemy po liście q
+    //wskaznik na element z listy q
+    lista obecnyQ = *q;
+
     do
     {
-        lista poprzedni = NULL; // Wskaźnik do poprzedniego elementu
-        lista obecny = *l;      // Iterujemy po liście l
-        do
+        //wskaznik na poprzedni element
+        lista poprzedni = NULL;
+        //wskaznik na obecny element listy l
+        lista obecny = *l;
+        //oznaczenia pierwszego przejscia przez liste
+        int pierwszaIteracja = 1;
+
+        //jak jest pierwsza iteracja albo obecny element nie jest rowny pierwszemu elementowi listy
+        while (pierwszaIteracja || obecny != *l)
         {
+            //oznaczenie ze nie jest juz to pierwsza iteracja
+            pierwszaIteracja = 0;
+
+            //jak znajde element listy L to przyrownuje go z elementem z listy Q
             if (obecny->klucz == obecnyQ->klucz)
             {
-                // Znaleziono element do usunięcia
+                //jak nie ma poprzedniego elementu to usuwam pierwszy element
                 if (poprzedni == NULL)
                 {
-                    // Usuwamy pierwszy element w liście
+                    // Usuwamy pierwszy element
                     lista usuwany = obecny;
-                    if (obecny->nast == obecny) // Jedyny element w liście
+                    //kiedy jest tylko 1 element w liście
+                    if (obecny->nast == obecny) 
                     {
                         *l = NULL;
+                        obecny = NULL;
+                        //lista staje sie pusta
                     }
+                    //kiedy jest wiecej niz 1 element
                     else
                     {
-                        // Przesunięcie głowy i aktualizacja cyklu
+                        //szukam ostatniego elementu
                         lista koniec = *l;
                         while (koniec->nast != *l)
                         {
                             koniec = koniec->nast;
                         }
+                        //przypisuje nastepny element jako pierwszy
                         koniec->nast = obecny->nast;
                         *l = obecny->nast;
+                        obecny = *l;
                     }
-                    obecny = obecny->nast;
+                    //usuwam element
                     free(usuwany);
                 }
+                //kiedy jest poprzedni element
                 else
                 {
-                    // Usuwanie środka lub końca
+                    //ustawiam elementy
                     poprzedni->nast = obecny->nast;
                     lista usuwany = obecny;
                     obecny = obecny->nast;
                     free(usuwany);
                 }
+
+                //znowu zaczynam od poczatku
+                //wiec dalej jest pierwsza iteracja
+                pierwszaIteracja = 1;
             }
+            //jak element nie jest rowny to ide dalej
             else
             {
-                // Przesuwanie wskaźników
                 poprzedni = obecny;
                 obecny = obecny->nast;
             }
-        } while (obecny != *l); // Przejście przez całą listę l
-        obecnyQ = obecnyQ->nast; // Przejście do kolejnego elementu w liście q
-    } while (obecnyQ != *q); // Przejście przez całą listę q
+
+            //kiedy l juz puste to koncze
+            if (*l == NULL)
+            {
+                break;
+            }
+        }
+        //po przejsciu calej listy L to ide do nastepnego elementu z listy Q
+        obecnyQ = obecnyQ->nast;
+    } while (obecnyQ != *q && *l != NULL); // Iteruj po liście `q` do końca lub dopóki `l` nie jest pusta
 }
