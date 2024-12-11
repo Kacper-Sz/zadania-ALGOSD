84065518ed766d25635e07b722c3d116a0166b04
Kacper_Szulc_322968 <80749286+Kacper-Sz@users.noreply.github.com>
2024-12-11 17:50:53 +0100

    xd

--- zadanie 7/funkcje.c
+++ zadanie 7/funkcje.c
@@ -4,216 +4,6 @@
 #include "funkcje.h"
 
 
-// dodanie wartownika
-void DodanieWartownika(lista *l)
-{
-    //alokacja pamieci dla wartownika
-    lista wartownik = malloc(sizeof(elListy));
-    wartownik->klucz = INT_MAX;
-    //dodam na koniec wiec pole nast to 0
-    wartownik->nast = 0;
-    //dopoki nie znajdziemy ostatniego elementu
-    while(*l)
-    {
-        l=&(*l)->nast;
-    }
-    //przypisanie ostatniego elementu jako wartownika
-    *l=wartownik;
-}
-
-
-//dodawanie dla listy uporzadkowanej
-void DodajDoPosortowanej(lista *l, int klucz)
-{
-    //alokacja pamieci dla nowego elementu
-    lista nowa = malloc(sizeof(elListy));
-    nowa->klucz = klucz;
-
-    //jesli lista jest pusta
-    if(*l == 0)
-    {
-        nowa->nast = *l;
-        *l = nowa;
-        return;
-    }
-
-    //dopoki nie znajdziemy elementu wiekszego od tego co chcemu dodac
-    while(*l != NULL && (*l)->klucz < klucz)
-    {
-        //adres czesci nastepnej
-        l = &((*l)->nast);
-    }
-
-    //dodajemy element przed elementem wiekszym
-    nowa->nast = *l;
-    *l = nowa;
-}
-
-//dodanie elementu do listy uporzadkowanej z wartownikiem
-void DodajDoPosortowanejWartownik(lista *l, int klucz)
-{
-    //alokacja pamieci dla nowego elementu
-    lista nowa = malloc(sizeof(elListy));
-    nowa->klucz = klucz;
-
-    //dopoki nie znajdziemy elementu wiekszego od tego co chcemu dodac
-    //zawsze bedzei wiekszy bo jest wartownik
-    while((*l)->klucz < klucz)
-    {
-        l = &((*l)->nast);
-    }
-
-    //dodajemy element przed elementem wiekszym
-    nowa->nast = *l;
-    *l = nowa;
-}
-
-
-//znajdowanie elementu w liscie uporzadkowanej
-lista* ZnajdzPosortowana(lista *l, int klucz)
-{
-    //dopoki nie znajdziemy elementu wiekszego od tego co chcemu dodac
-    while((*l) != NULL && (*l)->klucz < klucz)
-    {
-        l = &((*l)->nast);
-    }
-    //jesli nie ma elementu lub jest wiekszy
-    if((*l) == NULL || (*l)->klucz > klucz)
-    {
-        return NULL;
-    }
-    else
-    {
-        return l;
-    }
-}
-
-//znajdowanie elementu w liscie uporzadkowanej z wartownikiem
-lista* ZnajdzPosortowanaWartownik(lista *l, int klucz)
-{
-    //dopoki nie znajdziemy elementu wiekszego od tego co chcemu dodac
-    while((*l)->klucz < klucz)
-    {
-        l = &((*l)->nast);
-    }
-    //jesli element jest rowny
-    if((*l)->klucz == klucz)
-    {
-        return l;
-    }
-    else
-    {
-        return NULL;
-    }
-}
-
-//usuwanie elementu z listy uporzadkowanej
-void UsunWskazanyPosortowana(lista *l, int number)
-{
-    //dopoki nie znajdziemy elementu wiekszego od tego co chcemu dodac
-    while((*l) != NULL && (*l)->klucz < number)
-    {
-        l = &((*l)->nast);
-    }
-    //jesli element jest rowny usuwamy
-    if((*l) != NULL && (*l)->klucz == number)
-    {
-        lista nowa = *l;
-        *l = (*l)->nast;
-        free(nowa);
-    }
-    else
-    {
-        printf("element %d nie znajduje sie na liscie.\n", number);
-    }
-}
-
-//usuwanie elementu z listy uporzadkowanej z wartownikiem
-void UsunWskazanyPosortowanaWartownik(lista *l, int number)
-{
-    //dopoki nie znajdziemy elementu wiekszego od tego co chcemu dodac
-    while((*l)->klucz < number)
-    {
-        //przejscie do nastepnej czesci adresu
-        l = &((*l)->nast);
-    }
-
-    //jesli element jest rowny usuwamy
-    if((*l)->klucz == number)
-    {
-        lista nowa = *l;
-        *l = (*l)->nast;
-        free(nowa);
-    }
-    else
-    {
-        printf("element %d nie znajduje sie na liscie.\n", number);
-    }
-}
-
-//znajdowanie pierwszego elementu
-lista* ZnajdzPierwszy(lista *l)
-{
-    //jesli lista jest pusta
-   if (l == NULL) {
-        //printf("Lista jest pusta.\n");
-        return NULL;
-    }
-    //zwraca pierwszy element
-    return l;
-}
-
-//znajdowanie pierwszego elementu z wartownikiem
-lista* ZnajdzPierwszyWartownik(lista *l)
-{
-    //zakladam ze jedyny element to wartownik
-    //czy nastepepny element jest nullem ten po wartowniku
-    if ((*l)->nast == NULL) {
-        //printf("Lista jest pusta.\n");
-        return NULL;
-    }
-    //zwraca pierwszy element
-    return l;
-}
-
-//znajdowanie ostatniego elementu
-lista* ZnajdzOstatni(lista *l)
-{
-    //jesli lista jest pusta
-    if (l == NULL) {
-        //printf("Lista jest pusta.\n");
-        return NULL;
-    }
-    //ide na koniec listy
-    while ((*l)->nast != NULL) {
-        l = &((*l)->nast);
-    }
-    //zwracam ostatni element
-    return l;
-}
-
-//znajdowanie ostatniego elementu z wartownikiem
-lista* ZnajdzOstatniWartownik(lista *l)
-{
-    //jesli lista jest pusta
-    if ((*l)->nast == NULL) {
-        //printf("Lista jest pusta.\n");
-        return NULL;
-    }
-    //ide na koniec listy
-    //sprawdzam czy element po wartowniku nie jest nullem
-    while ((*l)->nast->nast != NULL) {
-        l = &((*l)->nast);
-    }
-    //zwracam ostatni element
-    return l;
-}
-
-
-
-
-
-
 //wyswietlanie listy
 
 void WyswietlListeOdPoczatku(lista l)
@@ -226,14 +16,72 @@ void WyswietlListeOdPoczatku(lista l)
     }
     while (nowa)
     {
-        if(nowa->klucz == INT_MAX)
-        {
-            printf("Wartownik");
-            nowa = nowa->nast;
-            continue;
-        }
         printf("%d ", nowa->klucz);
         nowa = nowa->nast;
     }
     printf("\n\n");    
+}
+
+
+//dodawanie dla listy uporzadkowanej
+void DodajDoPosortowanej(lista *l, int number)
+{
+    //alokacja pamieci dla nowego elementu
+    lista nowa = malloc(sizeof(elListy));
+    nowa->klucz = number;
+
+    //jesli lista jest pusta
+    if(*l == 0)
+    {
+        nowa->nast = *l;
+        *l = nowa;
+        return;
+    }
+
+    //dopoki nie znajdziemy elementu wiekszego od tego co chcemu dodac
+    while(*l != NULL && (*l)->klucz < number)
+    {
+        //adres czesci nastepnej
+        l = &((*l)->nast);
+    }
+
+    //dodajemy element przed elementem wiekszym
+    nowa->nast = *l;
+    *l = nowa;
+}
+
+//laczenie list
+void polaczListy(lista *l, lista *q)
+{
+    lista *k, t;
+    k = q;
+
+    while (*q != NULL)
+    {
+        while(*l != NULL && (*l)->klucz <= (*k)->klucz)
+        {
+            l = &((*l)->nast);
+        }
+
+        //kiedy lista l jest pusta
+        if(*l == NULL)
+        {
+            *l = *q;
+            *q = NULL;
+        }
+        else
+        {
+            while(*k != NULL && (*k)->klucz <= (*l)->klucz)
+            {
+                k = &((*k)->nast);
+            }
+            t = *l;
+            *l = *q;
+            *q = *k;
+            *k = t;
+            l = k;
+            k = q;
+        }
+    }
+    
 }
--- zadanie 7/zadanie7.c
+++ zadanie 7/zadanie7.c
@@ -16,20 +16,13 @@ int main()
         printf("------------------------------------\n");
         printf("nastepna operacja: \n");
 
-        printf("1 - wyswietl zawartosc listy\n");
+        printf("1 - wyswietl zawartosc list\n");
 
-        printf("2 - dodaj wartownik\n");
+        printf("2 - dodaj element do L\n");
+        printf("3 - dodaj element do Q\n");
 
-        printf("3 - dodaj element lista uporzadkowana\n");
-        printf("4 - dodaj element lista uporzadkowana z wartownikiem\n");
-        printf("5 - znajdz element w liscie uporzadkowanej\n");
-        printf("6 - znajdz element w liscie uporzadkowanej z wartownikem\n");
-        printf("7 - usun wskazany element z listy uporzadkowanej\n");
-        printf("8 - usun wskazany element z listy uporzadkowanej z wartownikiem\n");
-        printf("9 - pobierz pierwszy element z z listy uporzadkowanej\n");
-        printf("10 - pobierz pierwszy element z z listy uporzadkowanej z wartownikiem\n");
-        printf("11 - pobierz ostatni element z z listy uporzadkowanej\n");
-        printf("12 - pobierz ostatni element z z listy uporzadkowanej z wartownikiem\n");
+        printf("4 - polacz listy\n");
+        
         printf("0 - wyjdz\n\n");
 
         printf("twoj wybor: ");
@@ -49,15 +42,13 @@ int main()
             case 1:
                 //wyswietlanie listy
                 printf("\n");
+                printf("lista L: ");
                 WyswietlListeOdPoczatku(_l);
+                printf("\nlista Q: ");
+                WyswietlListeOdPoczatku(_q);
+                printf("\n");
                 break;
             case 2:
-                // dodaj wartownik
-                printf("\n");
-                DodanieWartownika(&_l);
-                printf("\n");
-                break;
-            case 3:
                 // dodaj element lista uporzadkowana
                 printf("\n");
                 printf("podaj liczbe ktora chcesz dodac: ");
@@ -65,98 +56,17 @@ int main()
                 DodajDoPosortowanej(&_l, number);
                 printf("\n");
                 break;
-            case 4:
-                // dodaj element lista uporzadkowana z wartownikiem
+            case 3:
+                // dodaj element lista uporzadkowana
                 printf("\n");
                 printf("podaj liczbe ktora chcesz dodac: ");
                 scanf("%d", &number);
-                DodajDoPosortowanejWartownik(&_l, number);
+                DodajDoPosortowanej(&_q, number);
                 printf("\n");
-                break;
-            case 5:
-                // znajdz element w liscie uporzadkowanej
-                printf("\n");
-                printf("podaj liczbe ktora chcesz znalezc: ");
-                scanf("%d", &number);
-                lista *znaleziony = ZnajdzPosortowana(&_l, number);
-                if (znaleziony) {
-                    printf("Znaleziono element %d\n", (*znaleziony)->klucz);
-                } else {
-                    printf("Nie znaleziono elementu %d\n", number);
-                }
-                printf("\n");
-                break;
-            case 6:
-                // znajdz element w liscie uporzadkowanej z wartownikiem
-                printf("\n");
-                printf("podaj liczbe ktora chcesz znalezc: ");
-                scanf("%d", &number);
-                lista *znalezionyWartownik = ZnajdzPosortowanaWartownik(&_l, number);
-                if (znalezionyWartownik) {
-                    printf("Znaleziono element %d\n", (*znalezionyWartownik)->klucz);
-                } else {
-                    printf("Nie znaleziono elementu %d\n", number);
-                }
-                printf("\n");
-                break;
-            case 7:
-                // usun wskazany element z listy uporzadkowanej
-                printf("\n");
-                printf("podaj liczbe ktora chcesz usunac: ");
-                scanf("%d", &number);
-                UsunWskazanyPosortowana(&_l, number);
-                printf("\n");
-                break;
-            case 8:
-                // usun wskazany element z listy uporzadkowanej z wartownikiem
-                printf("\n");
-                printf("podaj liczbe ktora chcesz usunac: ");
-                scanf("%d", &number);
-                UsunWskazanyPosortowanaWartownik(&_l, number);
-                printf("\n");
-                break;
-            case 9:
-                // pobierz pierwszy element z z listy uporzadkowanej
-                printf("\n");
-                lista* pierwszy = ZnajdzPierwszy(&_l);
-                if (pierwszy) {
-                    printf("Pierwszy element to %d\n", (*pierwszy)->klucz);
-                } else {
-                    printf("Lista jest pusta\n");
-                }
-                printf("\n");
-                break;
-            case 10:
-                // pobierz pierwszy element z z listy uporzadkowanej z wartownikiem
-                printf("\n");
-                lista* pierwszyWartownik = ZnajdzPierwszyWartownik(&_l);
-                if (pierwszyWartownik) {
-                    printf("Pierwszy element to %d\n", (*pierwszyWartownik)->klucz);
-                } else {
-                    printf("Lista jest pusta\n");
-                }
-                printf("\n");
-                break;
-            case 11:
-                // pobierz ostatni element z z listy uporzadkowanej
-                printf("\n");
-                lista* ostatni = ZnajdzOstatni(&_l);
-                if (ostatni) {
-                    printf("Ostatni element to %d\n", (*ostatni)->klucz);
-                } else {
-                    printf("Lista jest pusta\n");
-                }
-                printf("\n");
-                break;
-            case 12:
-                // pobierz ostatni element z z listy uporzadkowanej z wartownikiem
-                printf("\n");
-                lista* ostatniWartownik = ZnajdzOstatniWartownik(&_l);
-                if (ostatniWartownik) {
-                    printf("Ostatni element to %d\n", (*ostatniWartownik)->klucz);
-                } else {
-                    printf("Lista jest pusta\n");
-                }
+                break;      
+            case 4:
+                // laczenie list
+                polaczListy(&_l, &_q);
                 printf("\n");
                 break;
             default:
